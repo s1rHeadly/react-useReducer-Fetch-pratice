273052900909d80b1e4c75a083c29e58c83be309
davidmchale <dmchale@squiz.net>
2023-12-05 12:59:40 +1100

    update

--- src/Item.js
+++ src/Item.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
 const Item = ({info}) => {
-  console.log(info)
-  const {name, email, phone, dob, picture} = info;
-
+ 
+  const {name, email, picture} = info;
   const image = picture?.large
   const {first, last, title} = name
 
   return (
     <div>
-    <h3>{`${first} ${last}`}</h3>
+    <h3>{`${title} ${first} ${last}`}</h3>
     <p>{email}</p>
       <img src={image} alt={first} />
     </div>
